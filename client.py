f055bcc23986fd83d353b9644893dd390768ddb1
ArtemPospeev <pospeev.artem@iloud.com>
2022-12-04 02:51:36 +0400

    completed connection client/server

--- client.py
+++ client.py
@@ -1,10 +1,59 @@
+import json
 from socket import SOCK_STREAM, AF_INET, socket
+import datetime
+import argparse
+import sys
 
-sock = socket(AF_INET, SOCK_STREAM)
-sock.connect(('localhost', 8002))
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', default='localhost')
+parser.add_argument('-p', default='7777')
+args = parser.parse_args()
 
-msg = 'Hi, server!'
-sock.send(msg.encode('utf-8'))
+ENCODING = 'UTF-8'
+ADDR = args.a
+try:
+    PORT = int(args.p)
+except ValueError:
+    sys.exit('Неверный аргумент. В порт должно передавать число')
 
-data = sock.recv(1000000)
-print(f'Message from server {data.decode("utf-8")}')
+
+def start_connection(addr=ADDR, port=PORT):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((addr, port))
+    return sock
+
+
+def send_presence_to_server(sock, username='my_name'):
+    time = datetime.datetime.now().timestamp()
+    message = {
+        "action": "presence",
+        "time": f"{time}",
+        "type": "status",
+        "user": {
+            "account_name": f'{username}',
+            "status": "online"
+        }
+    }
+    sock.send(json.dumps(message).encode(ENCODING))
+
+
+def decode_answer_from_server(answer):
+    result = json.loads(answer.decode(ENCODING))
+    return result
+
+
+def get_answer_from_server(sock):
+    data = sock.recv(10000)
+    result = decode_answer_from_server(data)
+    return result
+
+
+def main():
+    sock = start_connection()
+    send_presence_to_server(sock)
+    answer = get_answer_from_server(sock)
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,15 +1,56 @@
 from socket import SOCK_STREAM, AF_INET, socket
-import time
+import json
+import datetime
+import argparse
+import sys
 
-sock = socket(AF_INET, SOCK_STREAM)
-sock.bind(('', 8002))
-sock.listen(5)
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', default='')
+parser.add_argument('-p', default=7777)
+args = parser.parse_args()
 
-while True:
-    client, addr = sock.accept()
-    data = client.recv(1000000).decode("utf-8")
-    print(f'Response accepted from: {addr}')
-    print(f'Data from client: {data}')
-    msg = 'Hi, client!'
-    client.send(msg.encode('utf-8'))
-    client.close()
+ENCODING = 'UTF-8'
+ADDR = args.a
+try:
+    PORT = int(args.p)
+except ValueError:
+    sys.exit('Неверный аргумент. В порт должно передавать число')
+
+
+def run_server(addr=ADDR, port=PORT):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((addr, port))
+    sock.listen(5)
+    return sock
+
+
+def decode_data_from_user(data):
+    result = json.loads(data.decode(ENCODING))
+    return result
+
+
+def send_response_to_client(client_, code_status=200):
+    time = datetime.datetime.now().timestamp()
+    json_answer = {
+        "response": f"{code_status}",
+        "time": f"{time}",
+    }
+    print(f'Отправлен ответ: {json_answer}')
+    client_.send(json.dumps(json_answer).encode(ENCODING))
+
+
+def main():
+    sock = run_server()
+    while True:
+        client, addr = sock.accept()
+        print('Подключение с адреса: ', addr)
+        print('-' * 30)
+        data = decode_data_from_user(client.recv(100000))
+        print(f'Получены данные: {data}')
+        print('-' * 30)
+        send_response_to_client(client)
+        client.close()
+
+
+if __name__ == '__main__':
+    main()
